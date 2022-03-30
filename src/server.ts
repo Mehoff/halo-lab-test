d83fb2c93fa86f780259a4ff2e4c9e5ad155691e
Mehoff <mixov2000@gmail.com>
2022-03-30 16:59:30 +0300

    Add typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
+import "reflect-metadata";
 import app from "./index";
-import db from "./db/pg";
 
 app.listen(process.env.APP_PORT, async () => {
   console.info(`Server is listening on port ${process.env.APP_PORT}`);
-
-  console.log("Hello from updated server.ts asdasdasdasdasd");
 });
